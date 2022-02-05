788b2ec3d7d7e0a96444719b888ad1870ba6ff13
Leonardo Molina <leomolina0501@hotmail.com>
2022-02-04 22:52:53 -0300

    Dia 4

---
+++ lib/app/routes/product_routers.dart
@@ -0,0 +1,13 @@
+import 'package:get/get_navigation/src/routes/get_route.dart';
+import 'package:vakinha_burger_mobile/app/modules/product_detail/product_detail_bindings.dart';
+import 'package:vakinha_burger_mobile/app/modules/product_detail/product_detail_page.dart';
+
+class ProductRouters {
+  ProductRouters._();
+  static final routers = <GetPage>[
+    GetPage(
+        name: '/product_detail',
+        binding: ProductDetailBindings(),
+        page: () => const ProductDetailPage()),
+  ];
+}
